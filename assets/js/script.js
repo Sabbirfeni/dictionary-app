da4be1e4f557e40d7f9514786d0a03ff7bbe7ecd
sabbir <contact.sabbirbhuiyan@gmail.com>
2023-12-01 21:34:21 +0600

    synonyms word fetching completed, close button works well, word audio set uped

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,24 @@ const wrapper = document.querySelector('.wrapper'),
 searchInput = wrapper.querySelector('input'),
 infoText = wrapper.querySelector('.info-text'),
 synonyms = wrapper.querySelector('.synonyms .list'),
-soundIcon = wrapper.querySelector('.word img');
+soundIcon = wrapper.querySelector('.word img'),
+removeIcon = wrapper.querySelector('.search .close-icon');
 let audio;
+
+
+const search = word => {
+
+    searchInput.value = word
+    fetchApi(word)
+}
 
 const data = (result, word) => {
     if(result.title) {
         infoText.innerHTML = `Can't find the meaning of <span>"${word}"</span>. Please try to search another word!`
     } else {
-        console.log(result)
         wrapper.classList.add('active')
-
         let definitions = result[0].meanings[0].definitions[0],
-        phonetics = `${result[0].meanings[0].partOfSpeech}${result[0].phonetics[1] !== undefined && result[0].phonetics[1].text}`;
+        phonetics = `${result[0].meanings[0].partOfSpeech}${result[0].phonetics[0] !== undefined && result[0].phonetics[0].text}`;
 
         document.querySelector('.word p').innerText = result[0].word;
         document.querySelector('.word span').innerText = phonetics;
@@ -34,7 +40,8 @@ const data = (result, word) => {
         //     searchInput.style.display = 'block'
         //     audio = new Audio(result[0].phonetics[0].audio);
         // }
-
+        console.log(definitions.example)
+        console.log(definitions.synonyms)
   
         if(definitions.example == undefined) {
             document.querySelector('.example').style.display = 'none';
@@ -42,17 +49,18 @@ const data = (result, word) => {
             document.querySelector('.example').style.display = 'block';
             document.querySelector('.example span').innerText = definitions.example;
         }
-   
 
         if(definitions.synonyms[0] == undefined) {
-            synonyms.parentElement.style.display = 'none';
+            synonyms.parentElement.parentElement.style.display = 'none';
         } else {
-            synonyms.parentElement.style.display = 'block';
+         
+            synonyms.parentElement.parentElement.style.display = 'block';
             synonyms.innerHTML = '';
-            for(let i = 0; i < 5; i++) {
-                let tag = `<span>${definitions.synonyms[i]}</span>`
-                synonyms.insertAdjacentHTML('beforeedn', tag);
-            }
+            const synonymsWords = definitions.synonyms;
+            synonymsWords.forEach(word => {
+                let tag = `<span onClick=search('${word}')>${word}</span>`
+                synonyms.insertAdjacentHTML('beforeend', tag);
+            })
         }
         
     }
@@ -61,6 +69,7 @@ const data = (result, word) => {
 const fetchApi = word => {
     infoText.style.color = '#000'
     infoText.innerHTML = `Searching the meaning of the word <span>"${word}"</span>`
+    wrapper.classList.remove('active');
     const API_URL = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
     fetch(API_URL).then(res => res.json()).then(result => {
         data(result, word)
@@ -80,4 +89,11 @@ searchInput.addEventListener('keyup', e => {
 
 soundIcon.addEventListener('click', () => {
     audio.play()
+})
+
+removeIcon.addEventListener('click', () => {
+    searchInput.value = '';
+    searchInput.focus();
+    infoText.innerHTML = `Type a word and press enter to get meaning, example, pronunciation, and synonyms of that typed word.`
+    wrapper.classList.remove('active');
 })
